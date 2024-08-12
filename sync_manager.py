b520b4daa83516943cb539c14c0df64a71681302
Timothy Lister <timothydlister@fastmail.com>
2024-08-12 13:52:27 -0700

    feat: Add time synchronization with server

--- sync_manager.py
+++ sync_manager.py
@@ -1,13 +1,24 @@
 import time
 import uuid
+import asyncio
 
 class SyncManager:
     def __init__(self):
         self.effect_counter = 0
+        self.time_offset = 0
 
     def generate_effect_id(self):
         self.effect_counter += 1
         return f"{time.time()}-{self.effect_counter}-{uuid.uuid4().hex[:8]}"
 
     def get_sync_time(self):
-        return time.time()
+        return time.time() + self.time_offset
+
+    async def sync_time_with_server(self):
+        # This method should be implemented to sync time with the server
+        # For now, we'll just log that it's been called
+        print("Time sync with server requested")
+        # In a real implementation, you would:
+        # 1. Send a request to the server for its current time
+        # 2. Calculate the time offset based on the server's response
+        # 3. Update self.time_offset
